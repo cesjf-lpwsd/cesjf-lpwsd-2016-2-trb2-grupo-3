d3f537aac58c98b145cfa9644a01f7201c8b96ee
vitorgduartelp@hotmail.com <aluno@L4MAQ21.labcesac.net>
2016-12-06 22:39:34 -0200

    Inclui tamplate. Inclui bean com o JSF.

--- src/java/controllers/NomeQualquer.java
+++ src/java/controllers/NomeQualquer.java
@@ -1,13 +1,18 @@
 
 package controllers;
 
+import java.io.IOException;
 import javax.annotation.Resource;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
+import javax.servlet.http.HttpServletResponse;
 import javax.transaction.UserTransaction;
 
 @ManagedBean(name="teste")
+@RequestScoped
 public class NomeQualquer {
     
     @PersistenceUnit(unitName= "jpa-web-pu")
@@ -16,9 +21,18 @@ public class NomeQualquer {
     @Resource(name = "java:comp/UserTransaction")
     UserTransaction ut;
     
-    public String teste(){
+    public void teste() throws IOException{
         System.out.println("teste");
-        return "teste";
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletResponse response = (HttpServletResponse)context.getExternalContext().getResponse();
+        response.sendRedirect("paginaInicial.xhtml");
+    }
+    
+    public void voltar() throws IOException{
+        System.out.println("teste");
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletResponse response = (HttpServletResponse)context.getExternalContext().getResponse();
+        response.sendRedirect("index.xhtml");
     }
     
 }
